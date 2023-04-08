6b160ee45d1441abd76ed0a3cbd3f4570337ab3e
TrangNgan248 <trangnganvirgo@gmail.com>
2023-04-08 21:12:48 +0700

    Updates

---
+++ precache-manifest.7c2efab982aa605895bd9ee2b26fa53c.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "6237a48d9f7448fdd3c4",
+    "url": "/youtubeClone/static/js/main.b9f88fbd.chunk.js"
+  },
+  {
+    "revision": "4db2c9f2c77079795cd3",
+    "url": "/youtubeClone/static/js/runtime~main.571fee9c.js"
+  },
+  {
+    "revision": "6c027e6804efc1481745",
+    "url": "/youtubeClone/static/js/2.26006f8c.chunk.js"
+  },
+  {
+    "revision": "5e306f5357d85307fbf99be42a3f20e8",
+    "url": "/youtubeClone/index.html"
+  }
+];
